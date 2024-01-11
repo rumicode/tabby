315cb4a33b2abe503bd6e9b4e6a460022ce96e06
Erfanium <erfanshield@outlook.com>
2024-01-11 19:01:54 +0330

    update

--- clients/vscode/src/jsonrpc/isRunningInsideAgent.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from 'vscode'
-
-export function isRunningInsideAgent(): boolean {
-    return vscode.workspace.getConfiguration().get<boolean>('cody.advanced.agent.running', false)
-}
